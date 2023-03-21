3308aedd473f15f414aa4c2329a9669f31ed6a05
ParkYunHo <qkrdbsgh0921@gmail.com>
2023-03-21 21:21:12 +0900

    init

---
+++ src/test/kotlin/com/yoonho/corespringsecurity/CoreSpringSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoonho.corespringsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
